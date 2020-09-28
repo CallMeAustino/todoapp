a3c50f091414f548c41f5b18d5630479936eeb57
Austin <austin.hokun.wong@gmail.com>
2020-09-27 20:31:54 -0400

    added todo endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,64 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const { default: TodosList } = require('../src/components/todos-list');
+const todoRoutes = express.Router();
 const PORT = 4000;
 
 app.use(cors()); //app.use binds middleware to the express server
 app.use(bodyParser.json());
+app.use('/todos', todoRoutes);
+
+/*get list of all todos*/
+todoRoutes.route('/').get(function(req, res) {
+    Todo.find(function(err, todos) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+})
+
+/*get a todo by id */
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
+        res.json(todo);
+    });
+});
+
+/*add a todo */
+todoRoutes.route('/add').post(function(req, res) {
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+/*update a todo*/
+todoRoutes.route('/update/:id').post(function(req, res) {
+    Todo.findById(req.params.id, function(err, todo) {
+        if (!todo)
+            res.status(404).send("data is not found");
+        else
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save().then(todo => {
+                res.json('Todo updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;
