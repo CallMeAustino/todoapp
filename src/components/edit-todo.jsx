d17d691db276b505952272a050e8dba9f48c2219
Austin <austin.hokun.wong@gmail.com>
2020-09-28 19:26:22 -0400

    fixed get request bug

--- src/components/edit-todo.jsx
+++ src/components/edit-todo.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-export default class EditTodo extends React.Component {
+export default class EditTodo extends Component {
     constructor(props) {
         super()
             
-            this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
-            this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
-            this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
-            this.onChangeTodoCompleted = this.onChangeTodoCompleted.bind(this);
-            this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+        this.onChangeTodoCompleted = this.onChangeTodoCompleted.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
 
             this.state = {
                 todo_description: '',
@@ -20,7 +20,7 @@ export default class EditTodo extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('http//localhost:4000/todos/'+this.props.match.params.id)
+        axios.get('http://localhost:4000/todos/'+this.props.match.params.id)
         .then(response => {
             this.setState({
                 todo_description: response.data.todo_description,
